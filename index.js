c0dc8a52db2d132f0c5679df6473fa1264d9c075
NicoFeld <nicofeldfeber@gmail.com>
2019-05-01 23:15:25 -0300

    Arreglo la funcion que reinicia las variables

--- index.js
+++ index.js
@@ -63,10 +63,10 @@ function montoVenta() {
 
 function reiniciarVariables() {
 	// TODO revisar esto
-	if ((T - cantidadDias) * 1440 >= 1440) {
+	if (T - cantidadDias * 1440 >= 1440) {
 		cantidadDias++;
 		SHTD = 0;
-		if ((cantidadDias - cantidadMeses) * 30 >= 30) {
+		if (cantidadDias - cantidadMeses * 30 >= 30) {
 			cantidadMeses++;
 			SFM = 0;
 		}
